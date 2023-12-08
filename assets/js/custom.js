0ff5f0ed4c611802517af5a84c250f8093e45b50
James Sing <87142332+ambivjaps@users.noreply.github.com>
2023-12-08 20:20:00 +0800

    Updated js for nav

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,28 +5,20 @@ const url = window.location.href;
 if (url.includes("dashboard")) {
   navLinks[0].classList.add("active");
 
-} else if (url.includes("tickets_index")) {
-  navLinks[1].classList.add("active");
-  navCollapse[0].classList.add("active");
-
-} else if (url.includes("tickets_create")) {
-  navLinks[1].classList.add("active");
-  navCollapse[1].classList.add("active");
-
-} else if (url.includes("tickets_view")) {
+} else if (url.includes("courses_my")) {
   navLinks[1].classList.add("active");
 
-} else if (url.includes("announcements")) {
+} else if (url.includes("courses_enrolled")) {
+  navLinks[2].classList.add("active");
+
+} else if (url.includes("progress")) {
+  navLinks[3].classList.add("active");
+
+} else if (url.includes("network")) {
   navLinks[4].classList.add("active");
 
-} else if (url.includes("faq")) {
+} else if (url.includes("profile")) {
   navLinks[5].classList.add("active");
-
-} else if (url.includes("about")) {
-  navLinks[6].classList.add("active");
-
-} else if (url.includes("settings")) {
-  navLinks[7].classList.add("active");
 
 } else {
   // navLinks[0].classList.remove("active");
